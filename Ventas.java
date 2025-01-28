c1949f6203de227cf63b7a21a09eab90c848a5de
GCesar57 <76024715+GCesar57@users.noreply.github.com>
2025-01-28 07:25:58 -0600

    Update Ventas.java

--- Ventas.java
+++ Ventas.java
@@ -10,29 +10,28 @@ public class Ventas {
 		 * mes. La tienda desea conocer aquellas ventas de mayores a $ 1000 y aquéllas
 		 * que son entre $800 y $1000 y las que son inferiores a $800.
 		 */
-		Scanner entrada = new Scanner(System.in);
-		int mayores=0, entre=0, menores=0;
-		int cantidad;
-		System.out.println("Ingresa el total de ventas del mes: ");
-		cantidad = entrada.nextInt();
-		int ventas[] = new int[cantidad];
-		for(int i=0; i<cantidad; i++) {
-			System.out.println("Ingrese el monto de la venta: ");
-			ventas[i] = entrada.nextInt();
-			if(ventas[i]<=1000) {
-				if(ventas[i]>=800) {
-					entre +=1;
-				}else {
-					menores +=1;
-				}//
-			}else {
-				mayores +=1;
-			}//
-		}//
-		System.out.println("Ventas mayores a $1000 :" + mayores);
-		System.out.println("Ventas entre $1000 y $800: " + entre);
-		System.out.println("Ventas menores a $800: " + menores);
-		entrada.close();
+		try(Scanner entrada = new Scanner(System.in)) {
+		    int mayores = 0, entre = 0, menores = 0;
+	            System.out.println("Ingresa el total de ventas del mes: ");
+	            int cantidad = entrada.nextInt();
+	            int[] ventas = new int[cantidad];
+	            for(int i = 0; i < cantidad; i++) {
+	            	System.out.println("Ingrese el monto de la venta: ");
+	                ventas[i] = entrada.nextInt();
+	                if(ventas[i] > 1000) {
+	                	mayores++;
+	                }else if(ventas[i] >= 800 && ventas[i] <= 1000) {
+	                	entre++;
+	                }else {
+	                	menores++;
+	                }//
+            }//
+            System.out.println("Ventas mayores a $1000: " + mayores);
+            System.out.println("Ventas entre $800 y $1000: " + entre);
+            System.out.println("Ventas menores a $800: " + menores);
+		} catch (Exception e) {
+			System.err.println("Error en la entrada de datos: " + e.getMessage());
+		}
 	}
 	//332
 
